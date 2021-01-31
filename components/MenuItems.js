24e635589d1fb90b779a3a8b51acf0ca30186e57
sahithimylangam <71957972+sahithimylangam@users.noreply.github.com>
2021-01-31 21:29:58 +0530

    Add files via upload

---
+++ components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'Subscription management',
+    path: '/marketing',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Billing Automation',
+    path: '/consulting',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Recurring Payments',
+    path: '/design',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Accounting & Taxes',
+    path: '/development',
+    cName: 'dropdown-link'
+  }
+];
